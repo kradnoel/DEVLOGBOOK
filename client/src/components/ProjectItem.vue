ce3ea589abd017d75dda32fab87e8e21ba1b5cea
Djamal dos Santos <leondark.tzk@gmail.com>
2020-03-26 15:09:42 +0200

    #3 Components Vue (ProjectItem, StatusItem)

---
+++ client/src/components/ProjectItem.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h2>{{ title }}</h2>
+		<p>{{ description }}</p>
+		<big>{{ timespent }} hours</big>
+	</div>
+</template>
+<style scoped>
+	p {
+		font-size: 14pt;
+	}
+</style>
+<script>
+	export default {
+		props: ['title', 'description', 'timespent'],
+		name: 'ProjectItem'
+	}
+</script>
---
+++ client/src/components/StatusItem.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<span>{{ value }}</span>
+		<p>{{ description }}</p>
+	</div>
+</template>
+<style scoped>
+	span {
+		font-size: 32pt;
+	}
+	p {
+		font-size: 16pt;
+	}
+</style>
+<script>
+	export default {
+		props: ['value', 'description'],
+		name: 'StatusItem'
+	}
+</script>
